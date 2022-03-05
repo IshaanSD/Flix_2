32cacc8a9c7585533702ad000d77b1afba25fc93
Ishaan Gupta <i3gupta@ucsd.edu>
2022-03-04 19:57:02 -0800

    Initial

---
+++ MovieCellTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieCellTableViewCell.swift
+//  Yelpy
+//
+//  Created by Ishaan Gupta on 2/25/22. sl
+//
+
+import UIKit
+
+class MovieCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Yelpy
+//
+//  Created by Ishaan Gupta on 2/26/22.
+//
+
+import UIKit
+
+class MovieGridCell:
+    UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
